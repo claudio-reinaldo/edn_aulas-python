537ce3a8508f1a108b93829a9d5f02e5501d6d6f
rscopim <ricardo.simines@gmail.com>
2025-03-06 11:57:44 -0300

    aula pratica

---
+++ pratica 1/diferença.py
@@ -0,0 +1,8 @@
+a = int(input("Digite um número: "))
+b = int(input("Digite um número: "))
+c = int(input("Digite um número: "))
+d = int(input("Digite um número: "))
+
+diferenca = (a * b) - (c * d)
+
+print(f"diferenca = {diferenca}")
